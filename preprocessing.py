d5496196a4001afb71423e02424906da7af786f7
Jason Lebov <jasonlebov@gmail.com>
2024-04-17 20:41:26 -0400

    Preprocessing Updates

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 # Drop Irrelevant Features - [‘Permissions’, ‘Music effects’, ‘Timestamp’]
@@ -12,9 +13,11 @@ def one_hot_encode(df, cols):
     return encoded_df
 
 # Label Encode Categorical Features
-def label_encode(df, cols):
-    label_encoder = LabelEncoder()
-    df[cols] = df[cols].apply(label_encoder.fit_transform)
+def label_encode(df, cols, options):
+    for feature in cols:
+        label_encoder = LabelEncoder()
+        label_encoder.fit(options)
+        df[feature] = label_encoder.transform(df[feature])
 
     return df
 
@@ -49,4 +52,53 @@ def pearson_matrix(df):
     corrDF = df.corr(method='pearson')
     return corrDF
 
+# Pearson Correlation --> Delta Feature & Remover
+def pearson_drop_delta(orig_df, corr_df, delta):
+    def delta_search(correlation_matrix, delta_val):
+        delta_list = [] 
+
+        val_len = len(correlation_matrix)
+
+        for row_val in range(val_len-1): 
+            for col_val in range(row_val+1, val_len): 
+                if np.abs(correlation_matrix.iloc[row_val, col_val]) > delta_val:
+                    options = [correlation_matrix.index[row_val], correlation_matrix.columns[col_val]] 
+                    delta_list.append(np.random.choice(options)) 
+
+        return delta_list
+
+    delta_list = delta_search(corr_df, delta) 
+    print(delta_list)
+    final_drop_list = list(set(delta_list)) 
+    print(final_drop_list)
+    final_DF = orig_df.drop(final_drop_list, axis=1)
+
+    return final_DF
+
+# Pearson Correlation --> Gamma Feature & Remover
+def pearson_drop_gamma(orig_df, corr_df, gamma):
+    def gamma_search(correlation_matrix, gamma_val):
+        gamma_list = [] 
+
+        target_col = corr_df.columns[-1] # Gets DF w/ only Target Column to Focus on... converts into Series Pandas Object (items())
+
+            # Iterates through every Cell from the Target Col
+        for row_val, corr_value in corr_df[target_col].items():
+            if np.abs(corr_value) < gamma: # Threshold Check
+                gamma_list.append(row_val) # Add Val Feature to 'gamma_list'
+
+        return gamma_list
+
+    gamma_list = gamma_search(corr_df, gamma) 
+    print(gamma_list)
+    final_drop_list = list(set(gamma_list)) 
+    print(final_drop_list)
+    # final_DF = orig_df.drop(final_drop_list, axis=1)
+
+    return None #final_DF
+
+# Update Sample Name --> Remove 'Symptoms_'
+def remove_prefix(df, col_name):
+    df[col_name] = df[col_name].str.replace('Symptoms_', '')
+    return df
 
